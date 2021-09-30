959d0c12a1c6510ad7605a47df622da71cc65ba5
elhadja <elhadja007@gmail.com>
2021-10-01 00:12:29 +0200

    Test update project details

--- PMBackend/src/test/resources/data.sql
+++ PMBackend/src/test/resources/data.sql
@@ -5,9 +5,8 @@ insert into user (id, pseudo, email) values (4, "elhadj4", "email4@test.com");
 
 insert into project (id, name) values (1, "project1");
 
-insert into user_project (user_id, project_id) values
-			(1, 1),
-			(2, 1),
-			(3, 1),
-			(4, 1);
+insert into user_project (user_id, project_id) 
+				values (1, 1), (2, 1), (3, 1), (4, 1);
+
 insert into project_managers (managed_projects_id, managers_id) values (1, 1);
+insert into project_managers (managed_projects_id, managers_id) values (1, 2);

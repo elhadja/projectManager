17148b99fbbb2f6291511dfbaa931c6e152586d9
elhadja <elhadja007@gmail.com>
2021-10-05 23:33:28 +0200

    Test accept project invitation

---
+++ src/test/java/com/elhadjium/PMBackend/integrationTests/services/UserServiceITest.java
@@ -0,0 +1,71 @@
+package com.elhadjium.PMBackend.integrationTests.services;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.util.Arrays;
+
+import javax.transaction.Transactional;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.context.jdbc.Sql;
+
+import com.elhadjium.PMBackend.Project;
+import com.elhadjium.PMBackend.dao.UserDAO;
+import com.elhadjium.PMBackend.dto.InviteUsersToProjectInputDTO;
+import com.elhadjium.PMBackend.entity.User;
+import com.elhadjium.PMBackend.service.UserService;
+
+@SpringBootTest
+@DirtiesContext(classMode = DirtiesContext.ClassMode.BEFORE_EACH_TEST_METHOD)
+@Tag("IntegrationTests")
+public class UserServiceITest {
+	@Autowired
+	UserService userService;
+	
+	@Autowired
+	UserDAO userDAO;
+
+	@Test
+	@DisplayName("createUserProject should: create a project + add the user to the project + set the user as manager")
+	@Transactional
+	public void createUserProject_ok() throws Exception {
+		// prepare
+		User user = new User();
+		user.setEmail("email");
+		user.setPassword("password");
+		user.setPseudo("pseudo");
+		userService.signup(user);
+		
+		Project projectToAdd = new Project();
+		projectToAdd.setName("name");
+
+		//when
+		userService.CreateUserProject(user.getId(), projectToAdd);
+		
+		//then
+		user = userDAO.findById(user.getId()).get();
+		assertEquals(1, user.getProjects().size());
+		assertEquals(projectToAdd.getName(), user.getProjects().get(0).getProject().getName());
+
+		assertEquals(1, user.getManagedProjects().size());
+		assertEquals(user.getManagedProjects().get(0).getName(), projectToAdd.getName());
+	}
+	
+	@Test
+	@Sql("/acceptInvitationToProjectOk.sql")
+	@Transactional
+	public void acceptInvitationToProject_ok() throws Exception {
+		// when
+		userService.acceptInvitationToProjects(new String[] {"1"}, 1L);
+		
+		// then
+		User invitedUser = userDAO.findById(2L).get();
+		assertEquals(0, invitedUser.getInvitationnToProject().size());
+		assertEquals(1, invitedUser.getProjects().size());
+	}
+}

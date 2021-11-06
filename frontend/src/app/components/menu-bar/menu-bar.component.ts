f1bc0f4a847cd0f000eec17001928428e79a345c
elhadja <elhadja007@gmail.com>
2021-11-06 17:44:56 +0100

    Show menuBar only if user are logged

--- frontend/src/app/components/menu-bar/menu-bar.component.ts
+++ frontend/src/app/components/menu-bar/menu-bar.component.ts
@@ -1,7 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { MenuItem } from 'primeng/api';
+import { Subscriber, Subscription } from 'rxjs';
+import { SubjectSubscriber } from 'rxjs/internal/Subject';
 import { PMConstants } from 'src/app/common/PMConstants';
+import { sessionManagerService } from 'src/app/services/sessionManager.service';
 
 @Component({
   selector: 'app-menu-bar',
@@ -10,9 +13,15 @@ import { PMConstants } from 'src/app/common/PMConstants';
 })
 export class MenuBarComponent implements OnInit {
   public items: MenuItem[];
+  public isVisible: boolean;
 
-  constructor(private router: Router) { 
+  constructor(private router: Router,
+             private sessionService: sessionManagerService) { 
     this.items = [];
+    this.isVisible = false;
+    this.sessionService.userLoggedEmitter.subscribe((isUserLogged) => {
+      this.isVisible = isUserLogged;
+    })
   }
 
   ngOnInit(): void {
@@ -33,7 +42,7 @@ export class MenuBarComponent implements OnInit {
           {
             label: 'quitter',
             command: () => { 
-              localStorage.clear;
+              this.sessionService.closeSession();
               this.router.navigateByUrl(PMConstants.AUTHENTICATION_MODULE_BASE_URI) ;
             }
           }
@@ -41,5 +50,4 @@ export class MenuBarComponent implements OnInit {
       }
     ]
   }
-
 }
--- frontend/src/app/services/sessionManager.service.ts
+++ frontend/src/app/services/sessionManager.service.ts
@@ -1,17 +1,21 @@
 import { Injectable } from "@angular/core";
+import { Subject } from "rxjs";
 import { PMConstants } from "../common/PMConstants";
 
 @Injectable()
 export class sessionManagerService {
     private userId: number
     private projectId: number;
+    public userLoggedEmitter: Subject<boolean>;
 
     constructor() {
         this.userId = -1;
         this.projectId = -1;
+        this.userLoggedEmitter = new Subject<boolean>();
     }
 
     public setUserid(id: number): void {
+        this.userLoggedEmitter.next(true);
         this.userId = id;
         localStorage.setItem(PMConstants.SESSION_USER_ID_KEY, `${id}`);
     }
@@ -41,5 +45,10 @@ export class sessionManagerService {
         }
 
         return this.projectId;
+    }
+
+    public closeSession(): void {
+        this.userLoggedEmitter.next(false);
+        localStorage.clear();
     }
 }

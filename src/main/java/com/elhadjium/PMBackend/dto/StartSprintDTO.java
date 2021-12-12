07154e0384d0ebc292ac8995675c351d022ac490
elhadja <elhadja007@gmail.com>
2021-12-12 16:46:06 +0100

    Fix start sprint

--- src/main/java/com/elhadjium/PMBackend/dto/StartSprintDTO.java
+++ src/main/java/com/elhadjium/PMBackend/dto/StartSprintDTO.java
@@ -1,6 +1,8 @@
 package com.elhadjium.PMBackend.dto;
 
-public class StartSprintDTO {
+import com.elhadjium.PMBackend.exception.PMInvalidInputDTO;
+
+public class StartSprintDTO implements DTOValidator {
 	private String startDate;
 	private String endDate;
 
@@ -21,6 +23,17 @@ public class StartSprintDTO {
 	public void setEndDate(String endDate) {
 		if (endDate != null) {
 			this.endDate = endDate.replace("T", " ").replace("Z", "");
+		}
+	}
+
+	@Override
+	public void validate() {
+		if (getStartDate() == null || getEndDate() == null) {
+			throw new PMInvalidInputDTO("start date and due date are required");
+		}
+		
+		if (startDate != null && endDate != null && startDate.equals(endDate)) {
+			throw new PMInvalidInputDTO("start date should be different with due date");
 		}
 	}
 }

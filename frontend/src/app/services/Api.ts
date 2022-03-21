5f6fb7dfe9116991c5cf8731d24769aa6152c7ea
elhadja <elhadja007@gmail.com>
2022-03-21 22:23:55 +0100

    Allow login with google

--- frontend/src/app/services/Api.ts
+++ frontend/src/app/services/Api.ts
@@ -1,82 +1,92 @@
-import { HttpClient, HttpErrorResponse, HttpHeaders } from "@angular/common/http";
-import { Injectable } from "@angular/core";
-import { Observable, throwError } from "rxjs";
+import { HttpClient, HttpErrorResponse, HttpHeaders } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable, throwError } from 'rxjs';
 import { catchError } from 'rxjs/operators';
-import { environment } from "src/environments/environment";
-import { PMConstants } from "../common/PMConstants";
-import { MessageService } from "./message.service";
+import { environment } from 'src/environments/environment';
+import { PMConstants } from '../common/PMConstants';
+import { MessageService } from './message.service';
 
 @Injectable()
 export class API {
-    private baseURI: string;
-    private httpOptions: {
+  private baseURI: string;
+  private httpOptions: {
         headers?: HttpHeaders;
-    };
+    } | undefined;
 
-    constructor(private httpClient: HttpClient, private messageService: MessageService) {
-        this.baseURI = environment.api_base_uri;
-        this.httpOptions = {
-            headers: new HttpHeaders({
-                'Content-Type': 'application/json',
-                'Accept-Language': 'fr', // TODO allow user to set language
-                'Authorization': ''
-            })
-        };
-        let token = localStorage.getItem(PMConstants.SESSION_TOKEN_ID_KEY);
-        if (token !== null) {
-            this.setHttpOptions(token);
-        }
+  constructor(private httpClient: HttpClient, private messageService: MessageService) {
+    this.baseURI = environment.api_base_uri;
+    this.httpOptions = undefined;
+    const token = localStorage.getItem(PMConstants.SESSION_TOKEN_ID_KEY);
+    this.setHttpOptions(token ?? '');
+  }
+
+  public postWithoutHeaders(uri: string, body: any): Observable<any> {
+    return this.httpClient.post(this.baseURI + uri, body)
+      .pipe(
+        catchError(this.f)
+      );
+  }
+
+  public post(uri: string, body: any): Observable<any> {
+    return this.httpClient.post(this.baseURI + uri, body, this.httpOptions)
+      .pipe(
+        catchError(this.f)
+      );
+  }
+
+
+  public put(uri: string, body: any): Observable<any> {
+    return this.httpClient.put(this.baseURI + uri, body, this.httpOptions).pipe(
+      catchError(this.f)
+    );
+  }
+
+  public get(uri: string): Observable<any> {
+    return this.httpClient.get(this.baseURI + uri, this.httpOptions).pipe(
+      catchError(this.f)
+    );
+  }
+  public delete(uri: string): Observable<any> {
+    return this.httpClient.delete(this.baseURI + uri, this.httpOptions).pipe(
+      catchError(this.f)
+    );
+  }
+
+  public setHttpOptions(token: string) {
+    if (this.httpOptions == null || this.httpOptions.headers == null) {
+      this.httpOptions = {
+        headers: new HttpHeaders({
+          'Content-Type': 'application/json',
+          'Accept-Language': 'fr',
+        })
+      };
+    } 
+
+    if (token != null && token != '') {
+      this.httpOptions.headers = this.httpOptions.headers?.set('Authorization', 'Bearer ' + token);
+    }
+  }
+
+  public clearHeader(): void {
+    if (this.httpOptions != null) {
+      this.httpOptions.headers = undefined;
+    }
+  }
+
+  // TODO handle errors properly
+  private f = (error: HttpErrorResponse): Observable<never> => {
+    if (error.status === 401) {
+      if (error.error != null) {
+        this.messageService.showErrorMessage(error.error.message);
+      } else {
+        this.messageService.showErrorMessage('Invalid login');
+      }
+    } else if (error.status === 500) {
+      this.messageService.showErrorMessage('Internal server error, please contact an administrator');
+    } else {
+      this.messageService.showErrorMessage(error.error.message);
     }
 
-    public postWithoutHeaders(uri: string, body: any): Observable<any> {
-        return this.httpClient.post(this.baseURI + uri, body)
-                    .pipe(
-                        catchError(this.f)
-                    );
-    }
-
-    public post(uri: string, body: any): Observable<any> {
-        return this.httpClient.post(this.baseURI + uri, body, this.httpOptions)
-                    .pipe(
-                        catchError(this.f)
-                    );
-    }
-
-
-    public put(uri: string, body: any): Observable<any> {
-        return this.httpClient.put(this.baseURI + uri, body, this.httpOptions).pipe(
-            catchError(this.f)
-        );
-    }
-
-    public get(uri: string): Observable<any> {
-        return this.httpClient.get(this.baseURI + uri, this.httpOptions).pipe(
-            catchError(this.f)
-        );
-    }
-    public delete(uri: string): Observable<any> {
-        return this.httpClient.delete(this.baseURI + uri, this.httpOptions).pipe(
-            catchError(this.f)
-        );
-    }
-
-    public setHttpOptions(token: string) {
-        this.httpOptions.headers = this.httpOptions.headers?.set("Authorization", "Bearer " + token);
-    }
-
-    public clearHeader(): void {
-        this.httpOptions.headers = undefined;
-    }
-
-    private f = (error: HttpErrorResponse): Observable<never> => {
-        if (error.status === 401 && !!error.error) {
-            this.messageService.showErrorMessage(error.error.message);
-        } else if (error.status === 500) {
-            this.messageService.showErrorMessage("Internal server error, please contact an administrator");
-        } else {
-            this.messageService.showErrorMessage(error.error.message);
-        }
-
-        return throwError(error);
-    }
+    return throwError(error);
+  };
 }

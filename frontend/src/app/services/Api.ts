13fa001a5cc9299f547f93ad60013343544f684d
elhadja <elhadja007@gmail.com>
2022-11-03 21:40:03 +0100

    Actualize HttpHeader when user Logout

--- frontend/src/app/services/Api.ts
+++ frontend/src/app/services/Api.ts
@@ -87,7 +87,7 @@ export class API {
 
   public clearHeader(): void {
     if (this.httpOptions != null) {
-      this.httpOptions.headers = undefined;
+      this.httpOptions.headers?.delete('Authorization');
     }
   }
 
